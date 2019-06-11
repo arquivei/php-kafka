b32c7261a303146274dfa65303141d6dca109813
Vitor Ruschoni <vitor.ruschoni@arquivei.com.br>
2019-06-11 13:47:45 -0300

    Add monolog

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -2,17 +2,31 @@
 
 namespace Kafka\Consumer\Log;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\JsonFormatter;
+
 class Logger
 {
+    private $logger;
+
+    public function __construct()
+    {
+        $handler = new StreamHandler("php://stdout");
+        $handler->setFormatter(new JsonFormatter());
+        $this->logger = new \Monolog\Logger('PHP-KAFKA-CONSUMER-ERROR');
+        $this->logger->pushHandler($handler);
+        $this->logger->pushProcessor(function ($record) {
+            $record['datetime'] = $record['datetime']->format('c');
+            return $record;
+        });
+    }
+
     public function error(?int $messageId, int $attempts, \Throwable $exception): void
     {
-        $error = json_encode([
-            'message' => 'PHP-KAFKA-CONSUMER-ERROR',
+        $this->logger->error('Error to consume message', [
             'message_id' => $messageId,
             'throwable' => $exception,
             'attempt' => $attempts,
-            'time' => date('Y-m-d H:i:s'),
         ]);
-        print $error . PHP_EOL;
     }
 }
